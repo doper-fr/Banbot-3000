ba232cfcaed9e6f45d297f4b277f5791b94512a2
Doper-official <getdoper@gmail.com>
2025-08-03 14:15:19 +0530

    keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,11 +1,15 @@
 from flask import Flask
 from threading import Thread
 
-app = Flask('')
+app = Flask(__name__)
 
 @app.route('/')
 def home():
     return "BanBot 3000 is alive!", 200
+
+@app.route('/ping')
+def ping():
+    return "pong", 200
 
 def run():
     app.run(host='0.0.0.0', port=8080)
